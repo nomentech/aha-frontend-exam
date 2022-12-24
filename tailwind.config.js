f07899d1a740011d10562df1f12714ab04baee0d
Nomad <nomad@Nomads-MacBook-Pro.local>
2022-12-24 13:41:13 +0800

    finish exam1, password input

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customblack: "#181818",
+        customgray: "#FFF8",
+        primary: "#00A3FF",
+        secondary: "#00D1FF"
+      }
+    },
   },
   plugins: [],
 }
